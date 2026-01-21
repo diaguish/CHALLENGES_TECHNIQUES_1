82174b2f80dcd21ff8b51e4b9e2bb5006e6dfe48
“DEBROISE <raxiou80@gmail.com>
2026-01-21 14:09:53 +0100

    Ignorer tout ce qui n'est pas nécéssaire

--- Livrables/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"application"},{"l":"cli"},{"l":"domain.exception"},{"l":"domain.model"},{"l":"domain.repository"},{"l":"infrastructures.database"},{"l":"infrastructures.filesystem"},{"l":"infrastructures.logging"},{"l":"infrastructures.security"}];updateSearchResults();
